93a4a9d6edeab3051d08d50fd9e0faf6d48007ca
René Schulze <kontakt@rene-schulze.info>
2021-09-05 02:57:34 +0200

    Remove chrome fix

--- virtualsky.js
+++ virtualsky.js
@@ -1190,12 +1190,7 @@ VirtualSky.prototype.getPhrase = function(key,key2){
 	}else return this.htmlDecode(this.lang[key]) || this.htmlDecode(this.langs.en[key]) || "";
 };
 VirtualSky.prototype.getDevicePixelRatio = function(){
-	// Prevent flickering in Chrome
-	if (window.chrome) {
-		return window.devicePixelRatio*2;
-	} else {
-		return window.devicePixelRatio;
-	}
+	return window.devicePixelRatio;
 };
 VirtualSky.prototype.resize = function(w,h){
 	if(!this.canvas) return;
